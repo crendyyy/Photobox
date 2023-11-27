19285bd1595a55cc8dc4c7c9ced777379446e03b
LackFos <80957646+LackFos@users.noreply.github.com>
2023-11-27 16:27:23 +0700

    Create dropdown component

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
-// import Dropdown from '../compenents/Layout/Dropdown'
+import React, { useState } from 'react'
+import Dropdown from '../compenents/Layout/Dropdown'
 
 const Dashboard = () => {
+  const paket = [
+    { id: 0, name: 'Semua Paket' },
+    { id: 1, name: 'Jomblo' },
+    { id: 2, name: 'Couple' },
+    { id: 3, name: 'Family' },
+  ]
+
+  const [typeFilter, setTypeFilter] = useState('Semua Paket')
+
   return (
     <>
       <header className='h-16 bg-white'></header>
@@ -10,10 +19,10 @@ const Dashboard = () => {
           <input
             type='text'
             placeholder='Cari Transaksi'
-            className='w-full h-10 max-w-2xl px-4 py-2 border rounded-lg outline-none border-slate-200'
+            className='w-full h-10 max-w-2xl px-4 py-2 border rounded-lg outline-none border-slate-200 focus:shadow-input focus:border-primary'
           />
 
-          {/* <Dropdown /> */}
+          <Dropdown options={paket} value={typeFilter} setValue={setTypeFilter} />
 
           <button className='px-4 ml-auto leading-10 text-white rounded-lg bg-primary whitespace-nowrap'>
             + Buat Booking
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#FFEDD5',
+          DEFAULT: '#F68C54',
+          dark: '#612505',
+        },
+      },
+      boxShadow: {
+        input: '0px 0px 0px 3px #FBE9DF;',
+      },
+    },
   },
   plugins: [],
-};
+}
