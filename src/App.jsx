3c891067458da6c695155aa10fd99e2c8c58438c
William Wijaya <141988558+crendyyy@users.noreply.github.com>
2023-11-27 14:32:00 +0700

    Create Calendar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
+import Calendar from 'react-calendar'
 import Banner from './compenents/Layout/Banner'
 import Deskripsi from './compenents/Layout/Deskripsi'
+
+import './Styles/react-calendar.css'
 
 const app = () => {
   return (
     <>
-      <div className='flex justify-center'>
-        <div className='w-[928px] border border-solid border-red-200 rounded-xl'>
-          <div className='flex w-[928px] h-16 border border-solid border-none items-center px-6 font '>
-            <h1 className='text-3xl font-black -skew-x-12'>LOGO</h1>
+      <div className='absolute translate-x-[-50%] translate-y-[-50%] flex flex-col items-center shadow-xl top-1/2 left-1/2 border border-none rounded-xl'>
+        <div className='w-[928px] border border-none rounded-xl bg-white'>
+          <div className='flex w-[928px] h-16 border border-none items-center px-6 font '>
+            <h1 className='font-black text-3xl -skew-x-12'>LOGO</h1>
           </div>
           <Banner />
-          <Deskripsi />
+          <div className='flex'>
+            <Deskripsi />
+            <Calendar />
+          </div>
         </div>
       </div>
     </>
--- src/compenents/Layout/Deskripsi.jsx
+++ src/compenents/Layout/Deskripsi.jsx
@@ -1,20 +1,33 @@
 import React from "react";
+import TimeCircle from "../../assets/Time";
+import Document from "../../assets/Document";
+import Image from "../../assets/Image";
+import Logo from "../../assets/Logo.svg";
 
 const Deskripsi = () => {
   return (
-    <div className="w-1/2 border-r border-solid border-black">
+    <div className="w-[46%] border border-none">
+      <div className="w-[120px] h-[120px] border border-none rounded-full bg-white flex absolute translate-x-[163.3%] translate-y-[-53.3%]">
+        <img src={Logo} alt="Logo" />
+      </div>
       <div className="flex justify-center pt-20 px-6 pb-8">
         <div className="flex justify-start flex-col w-[416px] gap-8">
-          <div>
-            <h1 className="text-2xl font-bold text-black mb-2">COMPANY NAME</h1>
-            <p className="text-base text-gray-300">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-2xl font-bold text-black">COMPANY NAME</h1>
+            <p className="text-base text-slate-500">
               The 1st Self-Photo Studio in Selatpanjang
             </p>
           </div>
           <div className="gap-4 flex flex-col">
-            <p>40 menit (pengambilan gambar bebas)</p>
-            <p>40 menit (pengambilan gambar bebas)</p>
-            <p>40 menit (pengambilan gambar bebas)</p>
+            <p className="flex text-sm text text-slate-700 gap-2">
+              <TimeCircle /> 40 menit (pengambilan gambar bebas)
+            </p>
+            <p className="flex text-sm text text-slate-700 gap-2">
+              <Image /> 3 lembar foto cetak
+            </p>
+            <p className="flex text-sm text text-slate-700 gap-2">
+              <Document /> Softcopy gratis
+            </p>
           </div>
         </div>
       </div>
