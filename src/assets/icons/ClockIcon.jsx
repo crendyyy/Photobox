533d89e6e452678beac231af3469daa953f5c75f
William Wijaya <141988558+crendyyy@users.noreply.github.com>
2023-11-30 11:24:09 +0700

    add icon

---
+++ src/assets/icons/ClockIcon.jsx
@@ -0,0 +1,23 @@
+const ClockIcon = () => {
+  return (
+    <svg
+      width='23'
+      height='24'
+      viewBox='0 0 23 24'
+      fill='none'
+      stroke='currentColor'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M20.3645 12.0006C20.3645 16.8967 16.396 20.8651 11.4999 20.8651C6.60371 20.8651 2.63525 16.8967 2.63525 12.0006C2.63525 7.10444 6.60371 3.13599 11.4999 3.13599C16.396 3.13599 20.3645 7.10444 20.3645 12.0006Z'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path d='M14.7887 14.8204L11.1758 12.6651V8.02002' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  )
+}
+export default ClockIcon
